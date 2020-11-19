30ed70b59c3746d80642e9ede87254e0ea78c088
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2020-11-19 15:24:14 -0700

    added comments

--- ProgrammingProject4/Form1.cs
+++ ProgrammingProject4/Form1.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Author: Dustin Johnson
+ * 
+ */
+
 namespace ProgrammingProject4
 {
     public partial class ticTacToeForm : Form
@@ -25,6 +30,7 @@ namespace ProgrammingProject4
 
         private void newGameButton_Click(object sender, EventArgs e)
         {
+            //initialize variables
             resultLabel.Text = " ";
             const int ROWS = 3;
             const int COLS = 3;
@@ -33,11 +39,14 @@ namespace ProgrammingProject4
             string draw = "Draw!";
             int x = 0;
             int o = 0;
-            int[,] gameGrid = new int[ROWS, COLS];
-            
 
+            //initialize 2D array for game
+            int[,] gameGrid = new int[ROWS, COLS];
+
+            // fill array 
             Generate(gameGrid);
 
+            // output array to labels
             output1.Text = LabelOutput(gameGrid[0, 0]);
             output2.Text = LabelOutput(gameGrid[0, 1]);
             output3.Text = LabelOutput(gameGrid[0, 2]);
@@ -48,12 +57,15 @@ namespace ProgrammingProject4
             output8.Text = LabelOutput(gameGrid[2, 1]);
             output9.Text = LabelOutput(gameGrid[2, 2]);
 
+            // checks for X wins
             if ((CheckAcrossTop(gameGrid) == 0) || (CheckAcrossMid(gameGrid) == 0) || (CheckAcrossBottom(gameGrid) == 0) ||
                 (CheckLeftDown(gameGrid) == 0) || (CheckMidDown(gameGrid) == 0) || (CheckRightDown(gameGrid) == 0) ||
                 (CheckDiagLeft(gameGrid) == 0) || (CheckDiagRight(gameGrid) == 0))
             {
                 x = 1;
-            } 
+            }
+
+            // checks for O wins
             if ((CheckAcrossTop(gameGrid) == 1) || (CheckAcrossMid(gameGrid) == 1) || (CheckAcrossBottom(gameGrid) == 1) ||
                 (CheckLeftDown(gameGrid) == 1) || (CheckMidDown(gameGrid) == 1) || (CheckRightDown(gameGrid) == 1) ||
                 (CheckDiagLeft(gameGrid) == 1) || (CheckDiagRight(gameGrid) == 1))
@@ -61,23 +73,31 @@ namespace ProgrammingProject4
                 o = 1;
             }
 
+            // display if neither won
             if (x == 0 && o == 0)
             {
                 resultLabel.Text = draw;
 
-            } else if (x == 1 && o == 0)
+            }
+            // display if X won
+            else if (x == 1 && o == 0)
             {
                 resultLabel.Text = xWin;
 
-            } else if (o == 1 && x == 0)
+            }
+            // display if O won
+            else if (o == 1 && x == 0)
             {
                 resultLabel.Text = oWin;
-            } else if (x == 1 && o == 1)
+            }
+            // displays if both X and O won
+            else if (x == 1 && o == 1)
             {
-                resultLabel.Text = "You're both winners in my book.";
+                resultLabel.Text = "You're both winners in my book XOXO";
             }
         }
 
+        // fill 2D int array with elements randomly with either 0s or 1s
         void Generate(int[,] grid)
         {
             Random rand = new Random();
@@ -91,6 +111,7 @@ namespace ProgrammingProject4
             }
         }
 
+        // uses input to determine whether element is an X or O
         string LabelOutput(int i)
         {
             string output = "";
@@ -105,14 +126,8 @@ namespace ProgrammingProject4
             return output;
         }
 
-        string ResultCheck(int[,] grid)
-        {
-            if (grid[0, 1] == 0)
-                return "womp";
-            else
-                return "bork bork bork";
-        }
-
+        // all check methods utilize an indicator to differentiate between X and O matches, returns 2 if no match found
+        // checks top row for matches
         int CheckAcrossTop(int[,] grid)
         {
             int indicator = 2;
@@ -125,11 +140,12 @@ namespace ProgrammingProject4
             {
                 return indicator = 1;
             }
-           
+
             return indicator;
 
         }
 
+        // checks middle row for matches
         int CheckAcrossMid(int[,] grid)
         {
 
@@ -143,12 +159,13 @@ namespace ProgrammingProject4
             {
                 return indicator = 1;
             }
-           
+
             return indicator;
 
 
         }
 
+        // checks bottom row for matches
         int CheckAcrossBottom(int[,] grid)
         {
             int indicator = 2;
@@ -161,10 +178,11 @@ namespace ProgrammingProject4
             {
                 return indicator = 1;
             }
-           
+
             return indicator;
         }
 
+        // checks left column for mathches
         int CheckLeftDown(int[,] grid)
         {
             int indicator = 2;
@@ -177,10 +195,11 @@ namespace ProgrammingProject4
             {
                 return indicator = 1;
             }
-           
+
             return indicator;
         }
 
+        // checks middle column for matches
         int CheckMidDown(int[,] grid)
         {
             int indicator = 2;
@@ -197,6 +216,7 @@ namespace ProgrammingProject4
             return indicator;
         }
 
+        // checks right column for matches
         int CheckRightDown(int[,] grid)
         {
             int indicator = 2;
@@ -209,30 +229,33 @@ namespace ProgrammingProject4
             {
                 return indicator = 1;
             }
-            
+
             return indicator;
         }
 
+        // checks diagonally top left to bottom right for matches
         int CheckDiagLeft(int[,] grid)
         {
             int indicator = 2;
 
-            if ((grid[0, 0] == 0) && grid[1, 1] == 0 && grid[2,2] == 0)
+            if ((grid[0, 0] == 0) && grid[1, 1] == 0 && grid[2, 2] == 0)
             {
                 indicator = 0;
-            } else if ((grid[0, 0] == 1) && (grid[1, 1] == 1) && (grid[2, 2] == 1))
+            }
+            else if ((grid[0, 0] == 1) && (grid[1, 1] == 1) && (grid[2, 2] == 1))
             {
                 return indicator = 1;
             }
 
-            return indicator;                
+            return indicator;
         }
 
+        // checks diagonally top right to bottom left for matches
         int CheckDiagRight(int[,] grid)
         {
             int indicator = 2;
 
-            if ((grid[0, 2] == 0) && (grid[1,1] == 0) && (grid[2, 0] == 0))
+            if ((grid[0, 2] == 0) && (grid[1, 1] == 0) && (grid[2, 0] == 0))
             {
                 indicator = 0;
             }
